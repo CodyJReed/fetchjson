7685071759d8aa7ac8e2a8452b7419a242fb119d
CodyJReed <cody2j2reed@gmail.com>
2019-09-07 20:18:35 -0400

    Added first typescript interface

--- index.ts
+++ index.ts
@@ -2,4 +2,22 @@ import axios from "axios";
 
 const url = "https://jsonplaceholder.typicode.com/todos/1";
 
-axios.get(url).then(res => console.log(res.data));
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+axios.get(url).then(res => {
+  const todo = res.data as Todo;
+
+  const id = todo.id;
+  const title = todo.title;
+  const completed = todo.completed;
+
+  console.log(`
+    The Todo with id: ${id}
+    Has a title of: ${title}
+    Is it finished? ${completed}
+  `);
+});
